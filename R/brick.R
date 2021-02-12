f003cb0955c5ede2fa36d0ad4f2d1e69819cad42
EvaMaeRey <evangeline.mae@gmail.com>
2021-02-11 21:30:53 -0500

    geom barbrick

--- R/brick.R
+++ R/brick.R
@@ -1,42 +1,81 @@
-# #####  Xmax ######
+StatBarbrick <- ggplot2::ggproto("StatBarbrick", ggplot2::Stat,
+
+                              compute_group = function(data, scales) {
+                                data$x %>%
+                                  data.frame(x = .) %>%
+                                  mutate(y = 1) %>%
+                                  mutate(group = 1:n())
+                              },
+
+                              required_aes = c("x")
+)
+
+
+
+GeomBarrr <- ggplot2::ggproto("GeomBarrr",
+                                        ggplot2::GeomBar,
+                                        default_aes = ggplot2::aes(colour = "grey35",
+                                                                   size = 0.3,
+                                                                   linetype = "solid",
+                                                                   alpha = 1,
+                                                                   fill = "grey85")
+)
+
+
+geom_barbrick <- function(mapping = NULL, data = NULL,
+                       position = "stack", na.rm = FALSE, show.legend = NA,
+                       inherit.aes = TRUE, ...) {
+  ggplot2::layer(
+    stat = StatBarbrick, geom = GeomBarrr, data = data, mapping = mapping,
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
+
+# ggplot(data = mtcars) +
+#   aes(x = factor(cyl)) +
+#   geom_barbrick() +
+#   facet_wrap(~ gear)
+
+
+
+# StatBarbrick <- ggplot2::ggproto("StatBarbrick", ggplot2::Stat,
 #
-# StatBrick <- ggplot2::ggproto("StatBrick", ggplot2::Stat,
-#                      compute_group = function(data, scales) {
+#                                  compute_group = function(data, scales) {
+#                                    data$x %>%
+#                                      data.frame(x = .) %>%
+#                                      # mutate(y = 1) %>%
+#                                      mutate(group = 1:n())
+#                                  },
 #
-#                          data.frame(x = data$x,
-#                                     group = 1:nrow(data))
-#
-#                        },
-#
-#                      required_aes = c("x")
+#                                  required_aes = c("x")
 # )
-#
-# # GeomSegmentdashed <- ggplot2::ggproto("GeomSegmentdashed", ggplot2::GeomSegment,
-# #   default_aes = ggplot2::aes(colour = "black", size = 0.5, linetype = "dashed",
-# #     alpha = NA)
-# #   )
-#
-# geom_brick <- function (mapping = NULL, data = NULL, stat = "bin", position = "stack",
-#                         ..., binwidth = NULL, bins = NULL, na.rm = FALSE, orientation = NA,
-#                         show.legend = NA, inherit.aes = TRUE)
+
+
+
+# geom_histbrick <- function (mapping = NULL, data = NULL, stat = "bin", position = "stack",
+#                             ..., binwidth = NULL, bins = NULL, na.rm = FALSE, orientation = NA,
+#                             show.legend = NA, inherit.aes = TRUE)
 # {
-#   layer(data = data, mapping = mapping, stat = stat, geom = GeomBar,
+#   layer(data = data, mapping = mapping, stat = stat, geom = GeomBarrr,
 #         position = position, show.legend = show.legend, inherit.aes = inherit.aes,
 #         params = list(binwidth = binwidth, bins = bins, na.rm = na.rm,
 #                       orientation = orientation, pad = FALSE, ...))
 # }
+
+
+# ggplot(data = mtcars) +
+#    aes(x = drat) +
+#    geom_histbrick(bins = 5)
 #
+# ggplot(data = mtcars) +
+#   aes(x = drat) +
+#   stat_bin(data = . %>% mutate(group = 1:n()), aes(group = group),
+#            color = "grey35", fill = "grey85")
 #
-# # ggplot(cars) +
-# #   aes(x = dist) +
-# #   geom_brick(group = -1L)
+# geom_histbrick <- function(){
 #
-# # geom_xmax <- function(mapping = NULL, data = NULL,
-# #                        position = "identity", na.rm = FALSE, show.legend = NA,
-# #                        inherit.aes = TRUE, ...) {
-# #   ggplot2::layer(
-# #     stat = StatXmax, geom = ggplot2::GeomSegmentdashed, data = data, mapping = mapping,
-# #     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-# #     params = list(na.rm = na.rm, ...)
-# #   )
-# # }
+#   stat_bin(data = . %>% mutate(group = 1:n()), aes(group = group),
+#                             color = "grey35", fill = "grey85")
+#
+# }
