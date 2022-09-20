1562f35217e2c0389bfbcd0c8c218c3deae496a2
EvaMaeRey <evangeline.mae@gmail.com>
2022-09-19 21:19:31 -0400

    sd in norm

--- R/geom_normal_dist.R
+++ R/geom_normal_dist.R
@@ -23,6 +23,8 @@ compute_group_dnorm <- function(data, scales, height = 1,
 
 
 
+
+
 StatDnorm <- ggplot2::ggproto("StatDnorm",
                               ggplot2::Stat,
                               compute_group = compute_group_dnorm,
@@ -60,6 +62,77 @@ geom_normal_dist <- function(mapping = NULL, data = NULL,
   ggplot2::layer(
     stat = StatDnorm,
     geom = ggplot2::GeomArea,
+    data = data,
+    mapping = mapping,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+
+    params = list(na.rm = na.rm, outline.type = outline.type,...)
+  )
+}
+
+
+
+########
+
+compute_group_dnorm_zlines <- function(data, scales, height = 1,
+                                       sd_min = -4, sd_max = 4) {
+
+  sd <- sd(data$x)
+  mean <- mean(data$x)
+
+
+  -4:4 %>%
+    tibble::tibble(x = .) %>%
+    dplyr::mutate(y = dnorm(x)*height/sd) %>%
+    dplyr::mutate(yend = 0) %>%
+    dplyr::mutate(x = x*sd + mean) %>%
+    dplyr::mutate(xend = x) %>%
+    dplyr::mutate(linetype = "dashed")
+
+}
+
+
+StatDnormzlines <- ggplot2::ggproto("StatDnormzlines",
+                                    ggplot2::Stat,
+                                    compute_group = compute_group_dnorm_zlines,
+                                    required_aes = c("x"))
+
+
+#' Fits normal distribution based on standard deviation
+#'
+#' @param mapping
+#' @param data
+#' @param position
+#' @param na.rm
+#' @param show.legend
+#' @param inherit.aes
+#' @param outline.type
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#' ggplot(faithful, aes(x = waiting)) +
+#'   geom_rug() +
+#'   geom_histogram(aes(y = ..density..)) +
+#'   geom_normal_dist(fill = "magenta") +
+#'   geom_normal_dist_zlines() +
+#'   facet_wrap(facets = vars( eruptions > 3 ), ncol = 1) +
+#'   geom_normal_dist(q_min = .99)
+#'
+#' ggplot(faithful, aes(x = waiting)) +
+#'   geom_density() +
+#'   geom_normal_dist(q_min = .05, q_max = 1-.05)
+geom_normal_dist_zlines <- function(mapping = NULL, data = NULL,
+                             position = "identity", na.rm = FALSE, show.legend = NA,
+                             inherit.aes = TRUE, outline.type = "upper", ...) {
+  ggplot2::layer(
+    stat = StatDnormzlines,
+    geom = ggplot2::GeomSegment,
     data = data,
     mapping = mapping,
     position = position,
--- R/stamp_normal_dist.R
+++ R/stamp_normal_dist.R
@@ -19,6 +19,9 @@ compute_group_norm_dist_stamp <- function(data, scales, mean = 0,
 
 }
 
+
+
+
 StampDnorm <- ggplot2::ggproto("StampDnorm",
                               ggplot2::Stat,
                               compute_group = compute_group_norm_dist_stamp,
@@ -69,6 +72,74 @@ stamp_normal_dist <- function(mapping = NULL, data = NULL,
 }
 
 
+compute_group_norm_dist_zlines_stamp <- function(data, scales, mean = 0,
+                                                 sd = 1, height = 1,
+                                                 sd_min = -4, sd_max = 4) {
+
+
+  -4:4 %>%
+    tibble::tibble(x = .) %>%
+    dplyr::mutate(y = dnorm(x)*height/sd) %>%
+    dplyr::mutate(yend = 0) %>%
+    dplyr::mutate(x = x*sd + mean) %>%
+    dplyr::mutate(xend = x) %>%
+    dplyr::mutate(linetype = "dashed")
+
+  # seq(sd_min, sd_max, .01) %>%
+  #   tibble::tibble(x = .) %>%
+  #   dplyr::mutate(y = dnorm(x)*height) %>%
+  #   dplyr::mutate(x = x*sd + mean) %>%
+  #   dplyr::mutate(alpha = .3)
+
+}
+
+StampDnormzlines <- ggplot2::ggproto("StampDnormzlines",
+                                     ggplot2::Stat,
+                                     compute_group = compute_group_norm_dist_zlines_stamp,
+                                     required_aes = c("x")
+)
+
+
+#' Title
+#'
+#' @param mapping
+#' @param data
+#' @param position
+#' @param na.rm
+#' @param show.legend
+#' @param inherit.aes
+#' @param outline.type
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#' library(tidyverse)
+#' ggxmean:::stamp_space() +
+#'   stamp_normal_dist(alpha = .5, height = 1, fill = "magenta") +
+#'   stamp_normal_dist(sd_min = -5, sd_max = -1.96, height = 1) +
+#'   stamp_normal_dist(q_min = 0.25, height = 1) +
+#'   stamp_normal_dist_zlines()
+#'
+#' ggxmean:::stamp_space() +
+#'   stamp_normal_dist(sd_min = -1, sd_max = 1, color = "slateblue",
+#'              color = "grey34", outline.type = "full") +
+#'   stamp_normal_dist(sd_min = -2, sd_max = 2, color = "slateblue", outline.type = "full") +
+#'   stamp_normal_dist(sd_min = -3, sd_max = 3, color = "slateblue", outline.type = "full") +
+#'   stamp_normal_dist(sd_min = -4, sd_max = 4, color = "slateblue", outline.type = "full") +
+#'   stamp_normal_dist(sd_min = -5, sd_max = 5, color = "slateblue", outline.type = "full")
+stamp_normal_dist_zlines <- function(mapping = NULL, data = NULL,
+                              position = "identity", na.rm = FALSE, show.legend = NA,
+                              inherit.aes = TRUE, outline.type = "upper", ...) {
+  ggplot2::layer(
+    stat = StampDnormzlines, geom = ggplot2::GeomSegment, data = data, mapping = mapping,
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+
+    params = list(na.rm = na.rm, outline.type = outline.type, ...)
+  )
+}
 
 
 StampPnorm <- ggplot2::ggproto("StampPnorm",
